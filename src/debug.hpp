90e6c1081ade7be64b1a232b331565dcc29c79c5
malaow3 <malaow3@yahoo.com>
2024-07-28 16:06:15 +0000

    Add defines for per-plaform system info

---
+++ src/debug.hpp
@@ -0,0 +1,7 @@
+#ifndef SRC_DEBUG_HPP_
+#define SRC_DEBUG_HPP_
+
+#undef __APPLE__
+#define __linux__
+
+#endif  // SRC_DEBUG_HPP_
--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,43 @@
 #include <fmt/format.h>
 
+#include <string>
+
+/*#include "./debug.hpp"*/
+
+using std::string;
+
+enum platform_t { macOS, Linux, Windows, Unknown };
+// to_string function
+string to_string(platform_t platform) {
+  switch (platform) {
+    case macOS:
+      return "macOS";
+    case Linux:
+      return "Linux";
+    case Windows:
+      return "Windows";
+    case Unknown:
+      return "Unknown";
+  }
+}
+
 #if defined(__APPLE__)
-const char platform[] = "macOS";
+#define PLATFORM_MACOS
+#include <sys/sysctl.h>
+#include <sys/types.h>
+const platform_t platform = macOS;
+
 #elif defined(__linux__)
-const char platform[] = "Linux";
+#define PLATFORM_LINUX
+const platform_t platform = Linux;
+
 #elif defined(_WIN32)
-const char platform[] = "Windows";
+#define PLATFORM_WINDOWS
+const platform_t platform = Windows;
+
 #else
-const char platform[] = "Unknown";
+#define PLATFORM_UNKNOWN
+const platform_t platform = Unknown;
 #endif
 
 struct version_t {
@@ -20,5 +50,41 @@ const version_t version = {0, 1, 0};
 
 int main(int argc, char **argv) {
   fmt::print("jo version {}.{}.{} - {}\n", version.major, version.minor,
-             version.patch, platform);
+             version.patch, to_string(platform));
+
+#ifdef PLATFORM_MACOS
+  char buffer[256];
+  size_t length = sizeof(buffer);
+  int error =
+      sysctlbyname("machdep.cpu.brand_string", buffer, &length, NULL, 0);
+  if (error != 0) {
+    switch (error) {
+      case ENOENT:
+        fmt::print("sysctlbyname failed with error {}\n", error);
+        break;
+      case EINVAL:
+        fmt::print("sysctlbyname failed with error {}\n", error);
+        break;
+      case EPERM:
+        fmt::print("sysctlbyname failed with error {}\n", error);
+        break;
+      default:
+        fmt::print("sysctlbyname failed with error {}\n", error);
+    }
+  }
+  fmt::print("{}\n", buffer);
+  return 0;
+#endif
+
+#ifdef PLATFORM_LINUX
+  return 1;
+#endif
+
+#ifdef PLATFORM_WINDOWS
+  return 1;
+#endif
+
+#ifdef PLATFORM_UNKNOWN
+  return 1;
+#endif
 }
